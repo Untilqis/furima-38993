7b16a99eddfcd2eb1febcdf84ca039cdfed1c685
Untilqis <haramakityuta2012@gmail.com>
2023-03-08 18:11:36 +0900

    itemsのモデルとテーブルの紐づけ

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,16 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  validates :name,             presence: true
+  validates :description,      presence: true
+  validates :category_id,      presence: true
+  validates :condition_id,     presence: true
+  validates :shipping_cost_id, presence: true
+  validates :prefecture_id,    presence: true
+  validates :shipping_date_id, presence: true
+  validates :price,            presence: true
+  validates :user,             presence: true
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
 
@@ -16,15 +26,5 @@ class Item < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :shipping_date
-
-  validates :name,             presence: true
-  validates :description,      presence: true
-  validates :category_id,      presence: true
-  validates :condition_id,     presence: true
-  validates :shipping_cost_id, presence: true
-  validates :prefecture_id,    presence: true
-  validates :shipping_date_id, presence: true
-  validates :price,            presence: true
-  validates :user,             presence: true
 
 end
