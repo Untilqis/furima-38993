92ae3ab74ef02488a0076f5de237f4ce1e7b1455
Untilqis <haramakityuta2012@gmail.com>
2023-03-15 13:47:01 +0900

    viewファイルへ移動できるように設定

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,2 +1,11 @@
 class PurchasesController < ApplicationController
+ 
+  def index
+    @purchase_address = PurchaseAddress.new
+  end
+
+  def create
+    binding.pry
+  end
+
 end
--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,4 +1,4 @@
-class PurchaseAdress
+class PurchaseAddress
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building, :phone_number, :purchase, :user_id, :item_id
 
@@ -15,6 +15,6 @@ class PurchaseAdress
   def save
     purchase = Purchase.create(user_id: user_id, item_id: item_id)
 
-    Adress.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building: building, phone_number: phone_number, purchase: purchase.id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building: building, phone_number: phone_number, purchase: purchase.id)
   end
 end
